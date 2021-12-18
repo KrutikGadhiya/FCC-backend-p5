d92a2a7632a7e134df0051e19ed6ba090ec5e69d
KrutikGadhiya <66325627+KrutikGadhiya@users.noreply.github.com>
2021-12-18 14:30:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size }= req.file
+  console.log(req.file)
+  res.json({name: originalname, type: mimetype, size})
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
